eba5b65a8988411a7fd47b69c32a69b02ab7afc4
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-05-29 11:24:33 -0300

    now you can choose icon

--- back-end/userFetchForIcon.js
+++ back-end/userFetchForIcon.js
@@ -12,3 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Nome do usuário não encontrado.");
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputFile = document.getElementById('insert');
+    const icon = document.getElementById('image');
+
+    inputFile.addEventListener('change', () => {
+        const file = inputFile.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                icon.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
