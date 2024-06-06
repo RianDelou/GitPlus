b5d445f38f01857d3404fbd97b55b97b38d81dbb
V1t1nh0 <142180844+VictorSantos674@users.noreply.github.com>
2024-06-06 19:23:37 -0300

    Tentando reverter edit

--- cypress/e2e/historia3.cy.js
+++ cypress/e2e/historia3.cy.js
@@ -12,21 +12,27 @@ describe('Deve verificar um filme/série se ele está no gênero certo', () => {
         cy.get('#image1').click()
     });
 
-    it('Deve verificar um filme/série se ele está no gênero certo', () => {
-        cy.wait(3000); 
-       
-        cy.get('.trailer-item').should('be.visible').first().within(() => {
-      
-          cy.get('.input-my-list').check();    // adiciono
-        
-          cy.get('.input-my-list').should('be.checked');
-        });
-    
+    it('Deve verificar se um filme/série está no gênero certo', () => {
         cy.wait(3000);
-    
-        cy.get('ul.trailers-list#my-list').should('be.visible').within(() => { // verifico se ele realmente foi adicionado
-      
-          cy.get('.trailer-item').should('exist');
+
+        cy.get('.trailer-item').should('be.visible').first().within(() => {
+            cy.get('.input-my-list').check(); // adiciono
+            cy.get('.input-my-list').should('be.checked');
         });
-      });
+
+        cy.wait(3000);
+
+        cy.get('ul.trailers-list#my-list').should('be.visible').within(() => {
+            cy.get('.trailer-item').should('exist');
+        });
+
+        // Verifica se os filmes/séries do gênero selecionado são exibidos dinamicamente
+        cy.get('.h3-sections').each(($genreSection) => {
+            cy.wrap($genreSection).click();
+            cy.wait(1000); // Espera para garantir que o conteúdo foi carregado
+            cy.wrap($genreSection).next().within(() => {
+                cy.get('.trailer-item').should('be.visible');
+            });
+        });
+    });
 });
